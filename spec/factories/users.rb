48df6f5306828fc90ab37cc9e1b136c1f4d79b17
Mydeen <mailtomydeen@gmail.com>
2019-06-16 18:28:28 +0400

    updated rspec and readme

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,8 @@ FactoryBot.define do
     name { "Test User" }
     email { "test@store.com" }
     password { "abcd123" }
+    phone_number { "529141782" }
+    country_code { "971" }
+    is_admin { true }
   end
 end
--- spec/support/helpers/session_helpers.rb
+++ spec/support/helpers/session_helpers.rb
@@ -1,10 +1,12 @@
 module Features
   module SessionHelpers
-    def sign_up_with(email, password, confirmation)
+    def sign_up_with(email, password, confirmation, country_code, phone_number)
       visit new_user_registration_path
       fill_in 'Email', with: email
       fill_in 'Password', with: password
       fill_in 'Password confirmation', :with => confirmation
+      fill_in 'user[country_code]', with: country_code
+      fill_in 'user[phone_number]', with: phone_number
       click_button 'Sign up'
     end
 
